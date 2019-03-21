42a9d31e5514a7bc1ed58fbed6c408a250d5575d
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-21 10:26:49 -0400

    Make the Logic

--- CarRetail/MainActivity.cs
+++ CarRetail/MainActivity.cs
@@ -16,6 +16,8 @@ namespace CarRetail
         RadioButton cashRdBtn, financeRdBtn;
         Button btnTotal;
         TextView tvTotalOutput;
+        string[] carNames = { "Mercedec", "BMW", "Toyota", "Honda", "Nissan", "Ford" };
+        double[] carPrice = { 120000, 90000, 70000, 67000, 55000, 73000 };
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -29,8 +31,48 @@ namespace CarRetail
             btnTotal = (Button)FindViewById(Resource.Id.totalBtn);
             tvTotalOutput = (TextView)FindViewById(Resource.Id.totalTv);
 
+            carModelEt.TextChanged += delegate {
+                int s = search();
+                if (s == -1)
+                {
+                    Toast.MakeText(this, "The car doesn't exist", ToastLength.Long).Show();
+                    carPriceEt.Text = "";
+                }
+                else
+                    carPriceEt.Text = carPrice[s].ToString();
+            };
+            btnTotal.Click += delegate {
+                double price = double.Parse(carPriceEt.Text);
+                if (cashRdBtn.Checked)
+                    price = price * 1.13;
+                else if (financeRdBtn.Checked)
+                    price = price * 1.23;
+                tvTotalOutput.Text = price.ToString();
+
+            };
 
         }
+        //function to search for an item in the array and returns its index or -1 if not exist
+        int search()
+        {
+            if (carModelEt.Text != "")
+            {
+                string name = carModelEt.Text;
+                for (int i = 0; i < carNames.Length; i++)
+                    if (name == carNames[i])
+                        return i;
+                return -1;
+            }
+            else
+            {
+                Toast.MakeText(this, "Pleasae enter a car name", ToastLength.Long).Show();
+                carPriceEt.Text = "";
+                return -1;
+            }
+
+        }
+
+    
 
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
