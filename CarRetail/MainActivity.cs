ff96c4fd9f7681c66eb29598a6ce2b7dc30ff9d7
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-21 09:46:28 -0400

    Start with the Logic

--- CarRetail/MainActivity.cs
+++ CarRetail/MainActivity.cs
@@ -12,11 +12,23 @@ namespace CarRetail
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        EditText carModelEt, carPriceEt;
+        RadioButton cashRdBtn, financeRdBtn;
+        Button btnTotal;
+        TextView tvTotalOutput;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+
+            carModelEt = (EditText)FindViewById(Resource.Id.carEt);
+            carPriceEt = (EditText)FindViewById(Resource.Id.priceEt);
+            cashRdBtn = (RadioButton)FindViewById(Resource.Id.cashRadioGrp);
+            financeRdBtn = (RadioButton)FindViewById(Resource.Id.financeRadioGrp);
+            btnTotal = (Button)FindViewById(Resource.Id.totalBtn);
+            tvTotalOutput = (TextView)FindViewById(Resource.Id.totalTv);
+
 
         }
 
